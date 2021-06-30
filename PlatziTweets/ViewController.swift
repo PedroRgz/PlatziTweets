aaafacad69c64bc5e79965aa2a7f97630bbd1707
Pedro Rodríguez <p_alex96@hotmail.com>
2021-06-29 22:20:32 -0500

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by Pedro Rodríguez on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
