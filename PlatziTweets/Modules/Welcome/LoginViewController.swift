85bb2eaf5dd243a49d47d3d3c2325f9b3a5ecf91
Pedro Rodríguez <p_alex96@hotmail.com>
2021-06-30 22:35:59 -0500

    LoginViewController modificado para mostrar el baner en caso de que el correo no se haya escrito

--- PlatziTweets/Modules/Welcome/LoginViewController.swift
+++ PlatziTweets/Modules/Welcome/LoginViewController.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import NotificationBannerSwift
 
 class LoginViewController: UIViewController {
     //MARK: -Outlets
     @IBOutlet weak var loginButton:UIButton!
+    @IBOutlet weak var emailTextField:UITextField!
+    @IBOutlet weak var passwordTextField:UITextField!
+    
+    //MARK: -Actions
+    @IBAction func loginBtnAction(){
+        performLogin()
+    }
     
     /*
      TODO:
-     agregar los textfield y action del botón
-     separar la acción en un metodo a parte para el action
+     -agregar los textfield y action del botón
+     -separar la acción en un metodo a parte para el action
      mostrar el banner de error en caso de que el textfield esté vació
      agregar banner para eventos en la contraseña
      agregar banner para evento de login
@@ -31,6 +39,16 @@ class LoginViewController: UIViewController {
         
         //para hacer el boton redondo
         loginButton.layer.cornerRadius = 25
+    }
+    
+    private func performLogin(){
+        guard let email = emailTextField.text, !email.isEmpty else {
+            NotificationBanner(title: "Error",
+                               subtitle: "Debes indicar tu Email",
+                               style: .warning).show()
+            
+            return
+        }
     }
 
 }
