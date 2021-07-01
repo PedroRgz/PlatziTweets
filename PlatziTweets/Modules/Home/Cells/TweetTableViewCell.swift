645324542c21383c471bce4eed26f5eb94c14a4b
Pedro Rodríguez <p_alex96@hotmail.com>
2021-07-01 15:12:57 -0500

    Creada la celda. Aún no cuenta con funcionabilidad total

---
+++ PlatziTweets/Modules/Home/Cells/TweetTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  TweetTableViewCell.swift
+//  PlatziTweets
+//
+//  Created by Pedro Rodríguez on 01/07/21.
+//
+
+import UIKit
+import Kingfisher
+
+class TweetTableViewCell: UITableViewCell {
+    
+    //MARK: -IBOutlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageImageView: UIImageView!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var daleLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupCellWith(post:Post){
+        nameLabel.text = post.author.names
+        nicknameLabel.text = post.author.nickname
+        messageLabel.text = post.text
+        
+        if post.hasImage{
+            //configurar imagen
+            imageImageView.kf.setImage(with: URL(string: post.imagenUrl))
+        }else{
+            imageImageView.isHidden = true
+        }
+    }
+    
+}
--- PlatziTweets/Modules/Home/HomeViewController.swift
+++ PlatziTweets/Modules/Home/HomeViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    /*
+     agregar tableview
+     crear carpeta de celdas
+     crear UITableViewCell -> diseñar la celda
+     crear método publico en donde se configura la celda
+     kingfish para modificar la imagen 
+     */
 
     override func viewDidLoad() {
         super.viewDidLoad()
